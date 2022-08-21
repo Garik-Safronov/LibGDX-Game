7628a626686901d0853f2df9de4de2977a6d7d54
Garik-S <garik.safronov@gmail.com>
2022-08-21 14:47:21 +0300

    hw2

---
+++ core/src/com/mygdx/game/GameAnimation.java
@@ -0,0 +1,59 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class GameAnimation {
+
+    private Texture img;
+    private Animation <TextureRegion> anm;
+    private float time;
+
+    public GameAnimation(String name, int col, int row, Animation.PlayMode playMode){
+        img = new Texture(name);
+
+        TextureRegion region = new TextureRegion(img);
+        int xCnt = region.getRegionWidth() / col;
+        int yCnt = region.getRegionHeight() / row;
+
+        TextureRegion[][] regions = region.split(xCnt, yCnt);
+        TextureRegion[] region1 = new TextureRegion[regions.length * regions[0].length];
+
+        int index = 0;
+        for (int i = 0; i < regions.length; i++) {
+            for (int j = 0; j < regions[0].length; j++) {
+                region1[index++] = regions[i][j];
+            }
+        }
+        anm = new Animation<TextureRegion>(1/15f, region1);
+        anm.setPlayMode(playMode);
+
+        time += Gdx.graphics.getDeltaTime();
+    }
+
+    public TextureRegion getFrame(){
+        return anm.getKeyFrame(time);
+    }
+
+    public void setTime(float time){
+        this.time += time;
+    }
+
+    public void zeroTime(){
+        this.time = 0;
+    }
+
+    public boolean isAnimationOver(){
+        return anm.isAnimationFinished(time);
+    }
+
+    public void setPlayMode(Animation.PlayMode playMode) {
+        anm.setPlayMode(playMode);
+    }
+
+    public void dispose(){
+        img.dispose();
+    }
+}
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,43 +3,66 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	int click;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("img12.jpg");
-	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(0.5f, 0.4f, 0.8f, 1);
+    private SpriteBatch batch;
+    private GameAnimation animation;
+    private float x = 0;
+    private float y = 0;
+    private boolean toRight = true;
 
-		float x = Gdx.input.getX() - img.getWidth()/2;
-		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        animation = new GameAnimation("bird.png", 5, 3, Animation.PlayMode.LOOP);
+    }
 
-		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
-			click++;
-		}
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 1, 1, 1);
+        animation.setTime(Gdx.graphics.getDeltaTime());
+//        float x = Gdx.input.getX() - animation.getFrame().getRegionWidth() / 2;
+//        float y = Gdx.graphics.getHeight() - Gdx.input.getY() - animation.getFrame().getRegionHeight() / 2;
 
-		Gdx.graphics.setTitle("Clicked " + click + " times");
+        if (x + animation.getFrame().getRegionWidth() >= Gdx.graphics.getWidth()){
+            toRight = false;
+        }
+        if (x <= 0){
+            toRight = true;
+        }
 
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.draw(img, x, y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+        if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+            toRight = true;
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+            toRight = false;
+        }
+
+        if(animation.getFrame().isFlipX() && toRight){
+            animation.getFrame().flip(true,false);
+        }
+        if(!animation.getFrame().isFlipX() && !toRight){
+            animation.getFrame().flip(true,false);
+        }
+
+        if(toRight){
+            x++;
+        } else {
+            x--;
+        }
+
+        batch.begin();
+        batch.draw(animation.getFrame(), x, y);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        animation.dispose();
+    }
 }
